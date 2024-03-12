4b2b91238cbb5f5b3b8a4a4e11f3b5da8981e5f4
chuongchuong <uccoi1033@gmail.com>
2024-03-12 11:08:40 +0700

    Initial Commit

---
+++ cmd/web/route.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/chuongchuong/bookings/pkg/config"
+	"github.com/chuongchuong/bookings/pkg/handler"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	// mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	
+	mux.Get("/", handler.Repo.Home)
+	mux.Get("/about", handler.Repo.About)
+
+	return mux
+}
