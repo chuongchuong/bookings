6a7031008cf13e8737e5dc45b89f15e5960b94e4
chuongchuong <uccoi1033@gmail.com>
2024-03-12 11:44:37 +0700

    commit

--- cmd/web/route.go
+++ cmd/web/route.go
@@ -18,9 +18,10 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
-	
 	mux.Get("/", handler.Repo.Home)
 	mux.Get("/about", handler.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
