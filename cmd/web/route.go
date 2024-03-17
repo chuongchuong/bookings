603bfe2033348b66247af2493d4abc67b4d31123
chuongchuong <uccoi1033@gmail.com>
2024-03-18 00:43:53 +0700

    update handlers, CSRF, JSON and AJAX

--- cmd/web/route.go
+++ cmd/web/route.go
@@ -14,12 +14,21 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
-	// mux.Use(WriteToConsole)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
 	mux.Get("/", handler.Repo.Home)
 	mux.Get("/about", handler.Repo.About)
+	mux.Get("/generals-quarters", handler.Repo.Generals)
+	mux.Get("/majors-suite", handler.Repo.Majors)
+
+	mux.Get("/search-availability", handler.Repo.Availability)
+	mux.Post("/search-availability", handler.Repo.PostAvailability)
+	mux.Post("/search-availability-json", handler.Repo.AvailabilityJSON)
+
+	mux.Get("/contact", handler.Repo.Contact)
+
+	mux.Get("/make-reservation", handler.Repo.Reservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
