52d350e08884968432926144f3d1320be58675d7
chuongchuong <uccoi1033@gmail.com>
2024-04-24 00:13:32 +0700

    Changing app to use command line flags

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-go build -o Project.exe ./cmd/web/
-Project.exe
+go build -o  Project.exe ./cmd/web/
+Project.exe -dbname=bookings -dbuser=postgres -dbpass=Chaxeaoc123 -cache=false -production=false
 
-@REM        
+@REM    .\run.bat
